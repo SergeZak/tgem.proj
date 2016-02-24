38ba66346fd9d4490c5084b0d6f55f6527192c44
SergeZak <sergezak9@gmail.com>
2016-02-24 15:53:42 +0200

    created migration and model for Features

---
+++ database/migrations/2016_02_24_122806_create_features_tbl.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeaturesTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('icon');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('features');
+    }
+}
--- resources/views/admin/features/index.blade.php
+++ resources/views/admin/features/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin_app')
 
+@section('title')
+    List Features
+@endsection
+
 @section('content')
     hello features
 @endsection
