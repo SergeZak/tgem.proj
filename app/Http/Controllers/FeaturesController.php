81a98a9ca40378dd799a48fd89af4137b063a480
SergeZak <sergezak9@gmail.com>
2016-02-24 17:28:01 +0200

    display Feature on admin side

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Feature;
 
 class FeaturesController extends Controller
 {
@@ -16,7 +17,8 @@ class FeaturesController extends Controller
      */
     public function index()
     {
-        return view('admin.features.index');
+        $features = Feature::all();
+        return view('admin.features.index', compact('features'));
     }
 
     /**
--- resources/views/admin/features/index.blade.php
+++ resources/views/admin/features/index.blade.php
@@ -5,5 +5,81 @@
 @endsection
 
 @section('content')
-    hello features
+    <!-- widget grid -->
+    <section id="widget-grid" class="">
+
+        <!-- row -->
+        <div class="row">
+
+            <!-- NEW WIDGET START -->
+            <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-0" data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false">
+                    <!-- widget options:
+								data-widget-colorbutton="false"
+								data-widget-editbutton="false"
+								data-widget-togglebutton="false"
+								data-widget-deletebutton="false"
+								data-widget-fullscreenbutton="false"
+								data-widget-custombutton="false"
+								data-widget-collapsed="true"
+								data-widget-sortable="false"
+
+								-->
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-table"></i> </span>
+                        <h2>Features</h2>
+
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+
+                        <!-- widget edit box -->
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+
+                        </div>
+                        <!-- end widget edit box -->
+
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th style="width:20%;">Feature Name (used as title)</th>
+                                    <th style="width:10%;">Icon</th>
+                                    <th>Feature Description</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($features as $f)
+                                        <tr>
+                                            <td>{{ $f->name }}</td>
+                                            <td><i class="fa {{ $f->icon }}"></i></td>
+                                            <td>{{ $f->text }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                        </div>
+                        <!-- end widget content -->
+
+                    </div>
+                    <!-- end widget div -->
+
+                </div>
+                <!-- end widget -->
+
+            </article>
+            <!-- WIDGET END -->
+
+        </div>
+        <!-- end row -->
+
+    </section>
+    <!-- end widget grid -->
 @endsection
